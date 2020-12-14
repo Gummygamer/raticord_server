b6b674ca55ece27a5a888ff41b01895de60a0599
André Castro <aisushin@gmail.com>
2020-12-14 11:35:57 -0300

    Add files via upload

--- helpers/auth.middleware.js
+++ helpers/auth.middleware.js
@@ -1,1 +1,25 @@
+const jwt = require("jsonwebtoken");
 
+exports.loggedIn = function (req, res, next) {
+    let token = req.header('Authorization');
+    if (!token) return res.status(401).send("Access Denied");
+
+    try {
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length).trimLeft();
+        }
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        
+        let req_url = req.baseUrl+req.route.path;
+        if((req_url.includes("users/:id") || req_url.includes("messages/:id")) && parseInt(req.params.id) !== verified.id){
+            return res.status(401).send("Unauthorized!");
+        }		
+        
+        req.user = verified;
+        next();
+    }
+    catch (err) {
+        res.status(400).send("Invalid Token");
+    }
+}
