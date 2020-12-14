c142773d0f81baab72a745f6c80ab03d740600a1
André Castro <aisushin@gmail.com>
2020-12-14 11:49:51 -0300

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,11 @@
+const router = require('express').Router();
+const loggedIn = require("../helpers/auth.middleware");
+const userController = require('../controllers/user.controller');
 
+// Register a new User
+router.post('/register', userController.register);
+
+// Login
+router.post('/login', userController.login);
+
+module.exports = router;
