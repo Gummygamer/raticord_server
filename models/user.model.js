c6eef8d9e3c6ef33a6bfc9a0ae7f6b4605504914
André Castro <aisushin@gmail.com>
2020-12-14 11:45:55 -0300

    Add files via upload

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,19 @@
+User.create = async (newUser) => {
+    let insert = await pool.query("INSERT INTO user SET ?", newUser);
+    if( insert.insertId ) {
+        return insert.insertId;
+    }
+    else {
+        return;
+    }
+};
 
+User.login = async (value) => {
+    let row = await pool.query(`SELECT * FROM user WHERE name = ?`, value);
+    if( row.length ) {
+        return row[0];
+    }
+    else {
+        throw new NotFoundError("User does not exist");
+    }
+};
