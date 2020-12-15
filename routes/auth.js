47dff61448fb8eff62b4eae842d29e3c38de2b06
André Castro <aisushin@gmail.com>
2020-12-15 09:10:20 -0300

    updating routes including messages

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const loggedIn = require("../helpers/auth.middleware");
 const userController = require('../controllers/user.controller');
+const messageController = require('../controllers/message.controller');
 
 // Register a new User
 router.post('/register', userController.register);
 
 // Login
 router.post('/login', userController.login);
+
+router.get('/messages',messageController.list);
 
 module.exports = router;
